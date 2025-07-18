1c2715a983f86f9909e834f6297447d5ded9fbe4
RedstoneExpert <57807443+RedstoneExpert@users.noreply.github.com>
2025-07-18 02:45:09 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,62 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.10"
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
     `maven-publish`
+    signing
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
-group = "de.jackBeBack"
-version = "1.0-SNAPSHOT"
+group = "com.closedbrain"
+version = "1.0.0"
+description = "A slightly modified version of the awesome kotlin wrapper made by JackBeBack."
 
 repositories {
     mavenCentral()
 }
 
-val ktor_version: String = "2.3.7"
+val ktorVersion: String = "3.2.2"
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-kotlin {
-    jvmToolchain(17)
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+}
+
+mavenPublishing {
+    coordinates(group.toString(), "kollama", version.toString())
+
+    pom {
+        name.set("KOllama")
+        description.set(project.description)
+        url.set("https://github.com/JackBeBack/Ollama-KotlinWrapper/")
+
+        licenses {
+            license {
+                name="MIT"
+                url.set("https://www.opensource.org/licenses/mit-license.php")
+            }
+        }
+
+        developers {
+            developer {
+                name.set("Jack BeBack")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/ClosedBrain/KOllama")
+        }
+    }
 }
