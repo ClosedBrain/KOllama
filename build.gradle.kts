9942caffb13007762c0a17f0057cc1236d963032
RedstoneExpert <lukavec.martin.iv@gmail.com>
2025-07-19 23:28:06 +0200

    Opened Ollama

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.closedbrain"
-version = "1.0.3"
+version = "1.0.4"
 description = "A slightly modified version of the awesome kotlin wrapper made by JackBeBack."
 
 repositories {
